c7981cc11a9ccf1e7ce4a773ee8ecacac74b4e30
petstar <petstar320@gmail.com>
2025-06-25 14:52:45 +0000

    Ajout de la classe professeur

---
+++ professeur.java
@@ -0,0 +1,32 @@
+packname Tp1;
+public class Professeur {
+
+    private String nom;
+     private String matiere;
+
+    public Professeur(String nom, String matiere) {
+        this.nom = nom;
+        this.matiere = matiere;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getmatiere() {
+        return matiere;
+    }
+
+    public void setmatiere(String matiere) {
+        this.matiere = matiere;
+    }
+
+    
+   
+
+    
+}
